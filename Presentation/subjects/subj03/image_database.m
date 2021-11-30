b7066f86f56362d206e8ee2bfc9d28f6916269b9
ban.hiroshi <ban.hiroshi@gmail.com>
2021-12-01 06:30:34 +0900

    Added voronoi images for sample codes.
    Signed-off-by: ban.hiroshi <ban.hiroshi@gmail.com>

--- Presentation/subjects/subj03/image_database.m
+++ Presentation/subjects/subj03/image_database.m
@@ -5,14 +5,14 @@
 imgdb.type='image'; % database type, 'image' (image file) or 'matlab'(matlab .mat file).
 imgdb.directory=fullfile(fileparts(mfilename('fullpath')),'..','..','..','images','voronoi_images'); % full path to the image files
 imgdb.presentation_size=[400,400]; % this is not the actual image size, all the images will be adjusted based on this value.
-imgdb.num=201; % the total number of images
+imgdb.num=51; % the total number of images
 
 % set image file names
 % img{1}-img{100}   : inphase images
 % img{101}-img{200} : outphase images
 imgcounter=1;
 for jj=1:1:2
-  for ii=1:1:100
+  for ii=1:1:25
     imgdb.img{imgcounter}={sprintf('img_01_03_%02d_%02d.png',ii,jj),sprintf('voronoi_%02d',ii),jj-1};
     imgcounter=imgcounter+1;
   end
--- Presentation/subjects/subj03/protocolfile.m
+++ Presentation/subjects/subj03/protocolfile.m
@@ -36,22 +36,24 @@
 % blocks{n}.sequence=(2xN matrix);
 % Then, the same randomization parameter (0) will be applied to both sequences. Please be careful.
 
+nimg=50; % constant, the number of voronoi images to be presented.
+
 blocks{1}.randomization=0;
 blocks{1}.sequence=201;
-blocks{1}.msec=16000;
+blocks{1}.msec=5000;
 blocks{1}.slicing=100;
 blocks{1}.repetitions=1;
 blocks{1}.name='The First Fixation';
 
-block_counter=1; n_each=20;
-for ii=1:1:100/n_each
+block_counter=1; n_each=10;
+for ii=1:1:nimg/n_each
   block_counter=block_counter+1;
   stim_array=n_each*(ii-1)+1:n_each*ii;
   stim_array=[stim_array;stim_array+100;stim_array;stim_array+100];
   stim_array=stim_array(:)';
 
   blocks{block_counter}.randomization=0;
-  blocks{block_counter}.sequence=Pad2Array(stim_array,201,0);
+  blocks{block_counter}.sequence=Pad2Array(stim_array,nimg+1,0);
   blocks{block_counter}.msec=repmat([200,100],1,numel(stim_array));
   blocks{block_counter}.slicing=100;
   blocks{block_counter}.repetitions=3;
@@ -60,7 +62,7 @@ for ii=1:1:100/n_each
   block_counter=block_counter+1;
   blocks{block_counter}.randomization=0;
   blocks{block_counter}.sequence=201;
-  blocks{block_counter}.msec=6000;
+  blocks{block_counter}.msec=3000;
   blocks{block_counter}.slicing=100;
   blocks{block_counter}.repetitions=1;
   blocks{block_counter}.name='Fixation Rest';
@@ -69,7 +71,7 @@ end
 % replace the final fixation rest as below.
 blocks{block_counter}.randomization=0;
 blocks{block_counter}.sequence=201;
-blocks{block_counter}.msec=16000;
+blocks{block_counter}.msec=5000;
 blocks{block_counter}.slicing=100;
 blocks{block_counter}.repetitions=1;
 blocks{block_counter}.name='The Final Fixation';
